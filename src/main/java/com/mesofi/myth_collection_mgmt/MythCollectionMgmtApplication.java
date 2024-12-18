0b0f603b8acf14be80892ed3f7aac6b5d7016375
brocoly-dev <rivasarmando271084@icloud.com>
2024-12-18 09:12:42 -0600

    Added inital project

---
+++ src/main/java/com/mesofi/myth_collection_mgmt/MythCollectionMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.mesofi.myth_collection_mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MythCollectionMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MythCollectionMgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mesofi/myth_collection_mgmt/MythCollectionMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesofi.myth_collection_mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MythCollectionMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
