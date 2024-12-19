a688e49d7542904586fe0d4381b4205a0e49b3c3
brocoly-dev <rivasarmando271084@icloud.com>
2024-12-19 06:48:12 -0600

    Added pom changes

---
+++ src/main/java/com/mesofi/myth/collection/mgmt/controller/MythCollectionController.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.mesofi.myth.collection.mgmt.controller;
+
+/**
+ * @author armandorivasarzaluz
+ */
+public class MythCollectionController {}
